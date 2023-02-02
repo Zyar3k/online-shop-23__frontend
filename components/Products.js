5765e3ce4c7c20b700f0cf8d9830b9539d592eb6
Arkadiusz Zygan <a.zygan@gmail.com>
2023-02-02 20:45:41 +0100

    Add products page and connect to data

---
+++ components/Products.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { Grid, GridItem, Card, Box } from "@chakra-ui/react";
+import Link from "next/link";
+
+import { data } from "@/utils/data";
+
+const Products = () => {
+  return (
+    <div>
+      <Grid templateColumns={{ base: "1fr", lg: "repeat(4,1fr)" }} gap={6}>
+        {data.products.map((product) => (
+          <GridItem key={product.id} colSpan={1}>
+            <Card>
+              <Link href={`/product/${product.id}`}>
+                <Image
+                  src={`/images${product.image}`}
+                  height={230}
+                  width={230}
+                  style={{ objectFit: "cover", height: "230px" }}
+                  alt={product.title}
+                />
+                <Box py={1} px={2}>
+                  <h3>{product.title}</h3>
+                  <div>{product.description}</div>
+                  <div>{product.price}£</div>
+                </Box>
+              </Link>
+            </Card>
+          </GridItem>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default Products;
