f2750237bd0bc09981291b7b7a2eacec1c9fbd09
Arkadiusz Zygan <a.zygan@gmail.com>
2023-02-03 20:34:24 +0100

    Add products to mongodb

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema(
   {
     id: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
+    title: { type: String, required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String, required: true },
