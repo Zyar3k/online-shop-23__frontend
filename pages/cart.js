e5412673eeaa84f5fa81ec6204b82e0cfbb2caa4
Arkadiusz Zygan <a.zygan@gmail.com>
2023-02-07 12:55:15 +0100

    Add styles to cart

--- pages/cart.js
+++ pages/cart.js
@@ -2,22 +2,43 @@ import { useContext } from "react";
 
 import CartItem from "@/components/CartItem";
 import { CartContext } from "@/context/CartReducer";
-import { Box, Text, Stack } from "@chakra-ui/react";
+import { Box, Text, Stack, Heading, Flex } from "@chakra-ui/react";
 
 const CartPage = () => {
   const { cart } = useContext(CartContext);
   return (
-    <Box>
+    <Box
+      maxW={{ base: "3xl", lg: "7xl" }}
+      max="auto"
+      px={{ base: "4", md: "8", lg: "12" }}
+      py={{ base: "6", md: "8", lg: "12" }}
+    >
       {cart?.length === 0 ? (
         <Text fontSize="xl" fontWeight="bold">
           Your Cart is empty
         </Text>
       ) : (
         <>
-          <Stack>
-            {cart?.length > 0 &&
-              cart.map((item) => <CartItem key={item.id} item={item} />)}
+          <Stack
+            as="section"
+            spacing={{ base: "8", lg: "14" }}
+            direction={{ base: "column", lg: "row" }}
+            align={{ lg: "flex-start" }}
+          >
+            <Stack flex="2" spacing={{ base: "6", lg: "10" }}>
+              <Heading as="h1" size="2xl">
+                Shopping Cart
+              </Heading>
+              <Stack spacing={"6"}>
+                {cart?.length > 0 &&
+                  cart.map((item) => <CartItem key={item.id} item={item} />)}
+              </Stack>
+            </Stack>
           </Stack>
+          <Flex direction="column" align="center" flex="1">
+            {/* TODO */}
+            Order component
+          </Flex>
         </>
       )}
     </Box>
