a4012543eee463db6dbaf855dadab091ad1145b9
Arkadiusz Zygan <a.zygan@gmail.com>
2023-02-09 22:41:11 +0100

    Add checkou steps

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,11 +1,38 @@
 import React from "react";
 import ShippingAddressForm from "@/components/forms/Shipping";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  Progress,
+  Stack,
+} from "@chakra-ui/react";
 
 const CheckoutPage = () => {
+  const dispatch = useDispatch();
+  const activeStep = useSelector((state) => state.currentStep);
+
+  const steps = [
+    { name: "Shipping", component: <ShippingAddressForm /> },
+    { name: "Payment", component: <div>Payment</div> },
+    { name: "Review", component: <div>Review</div> },
+  ];
   return (
-    <div>
-      <ShippingAddressForm />
-    </div>
+    <Stack spacing={4}>
+      <Progress value={activeStep} max={steps.length} />
+      <Box mx="auto">
+        <Card w="xl" m="auto">
+          <CardHeader>
+            <Heading>{steps[activeStep].name}</Heading>
+          </CardHeader>
+          <CardBody>{steps[activeStep].component}</CardBody>
+        </Card>
+      </Box>
+    </Stack>
   );
 };
 
