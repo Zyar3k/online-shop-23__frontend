fb9353c91a260346570be4b1a41a6ff3e0df683e
Arkadiusz Zygan <a.zygan@gmail.com>
2023-02-10 20:53:59 +0100

    Add order model

---
+++ models/Order.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        description: { type: String, required: true },
+        image: { type: String, required: true },
+      },
+    ],
+    shippingAddress: {
+      fullName: { type: String, required: true },
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postcode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: { type: String, required: true },
+    itemsPrice: { type: Number, required: true },
+    shippingPrice: { type: Number, required: true },
+    taxPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+    isPaid: { type: Boolean, required: true },
+    isDelivered: { type: Boolean, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.models.Order || mongoose.model("Order", orderSchema);
+
+export default Order;
